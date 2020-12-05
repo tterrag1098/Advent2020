377680a8be44b7507daa7ab5724612a00b4d7a5e
tterrag <tterrag1098@gmail.com>
2020-12-05 00:51:29 -0500

    Day 5 take 2 iteration 2

--- src/main/java/com/tterrag/advent2020/taketwo/Day05.java
+++ src/main/java/com/tterrag/advent2020/taketwo/Day05.java
@@ -8,8 +8,16 @@ public class Day05 extends Day {
 
     @Override
     protected Result doParts() {
-        BitSet seats = lines().mapToInt(s -> Integer.parseInt(s.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
-                .collect(BitSet::new, (s, i) -> s.set(i), (i1, i2) -> i1.or(i2));
-        return new Result(seats.previousSetBit(0xFFF), seats.stream().filter(i -> seats.get(i) && !seats.get(i + 1) && seats.get(i + 2)).findAny().orElse(-2) + 1);
+        BitSet seats = lines().mapToInt(s -> {
+            int res = 0;
+            for (int i = 0; i < s.length(); i++) {
+                int c = s.charAt(i);
+                int val = (c >> 2) & 0b1;
+                if (((c >> 3) & 0b11) == 0) val ^= 1;
+                res = (res << 1) | val;
+            }
+            return res;
+        }).collect(BitSet::new, (s, i) -> s.set(i), (i1, i2) -> i1.or(i2));
+        return new Result(seats.previousSetBit(0xFFF), seats.stream().filter(i -> seats.get(i) && !seats.get(i + 1) && seats.get(i + 2)).findAny().orElse(-1));
     }
 }
